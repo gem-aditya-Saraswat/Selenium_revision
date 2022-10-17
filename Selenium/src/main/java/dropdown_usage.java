1cf59913f236f7f5d1712f2be314c5c808c8f9f9
gem-aditya-Saraswat <103404512+gem-aditya-Saraswat@users.noreply.github.com>
2022-10-17 11:54:02 +0530

    Dropdown assignment uploaded

---
+++ Selenium/src/main/java/dropdown_usage.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class dropdown_usage {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Assignments\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        //Finding the elements on which we want to perform actions.
+        WebElement username = driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+
+
+        //The action which we want to perform
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        button.click();
+
+        //Dropdown Implementation
+        Select drpdown = new Select(driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select")));
+        drpdown.selectByIndex(2);
+
+        //Item add to cart
+        WebElement itemadd = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-onesie\"]"));
+        itemadd.click();
+
+        driver.wait(10000);
+        driver.close();
+    }
+
+}
